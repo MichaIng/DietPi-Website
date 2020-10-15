fd6bdc0592eddaacb81312bdc835b863e6e476a9
MichaIng <micha@dietpi.com>
2020-10-16 00:55:34 +0200

    Simplify show/hide portfolio function

--- js/app.js
+++ js/app.js
@@ -132,35 +132,14 @@ $(function () {
 		});
 	// });
 
-	// Function for show or hide portfolio description
-	$.fn.showHide = function (options) {
-		var defaults = {
-			speed: 1000,
-			easing: '',
-			changeText: 0,
-			showText: 'Show',
-			hideText: 'Hide'
-		};
-		var options = $.extend(defaults, options);
-		$(this).on('click', function () {
-			$('.toggleDiv').slideUp(options.speed, options.easing);
-			var toggleClick = $(this);
-			var toggleDiv = $(this).attr('rel');
-			$(toggleDiv).slideToggle(options.speed, options.easing, function () {
-				if (options.changeText == 1) {
-					$(toggleDiv).is(':visible') ? toggleClick.text(options.hideText) : toggleClick.text(options.showText);
-				}
-			});
-			return false;
-		});
-	};
-
-	// Initial Show/Hide portfolio element
+	// Initially hide portfolio description
 	$('div.toggleDiv').hide();
-	$('.show_hide').showHide({
-		speed: 500,
-		changeText: 0,
-		showText: 'View',
-		hideText: 'Close'
+
+	// Show or hide portfolio description on click
+	$('.show_hide').on('click', function () {
+		$('.toggleDiv').slideUp(500, '');
+		var target = $(this).attr('rel')
+		$(target).slideToggle(500, '');
+		return false;
 	});
 });
