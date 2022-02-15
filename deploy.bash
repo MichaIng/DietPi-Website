0fd6c63169835abd48959dcd01777eb6f618af89
MichaIng <micha@dietpi.com>
2022-02-15 15:02:31 +0100

    Further updates
    also update and fix ROCK Pi S image, which had the wrong size

--- deploy.bash
+++ deploy.bash
@@ -33,7 +33,7 @@ if [[ -f '/boot/dietpi/func/dietpi-globals' ]]
 then
 	. /boot/dietpi/func/dietpi-globals
 else
-	curl -sSfL 'https://raw.githubusercontent.com/MichaIng/DietPi/master/dietpi/func/dietpi-globals' -o /tmp/dietpi-globals || exit 1
+	curl -sSf 'https://raw.githubusercontent.com/MichaIng/DietPi/master/dietpi/func/dietpi-globals' -o /tmp/dietpi-globals || exit 1
 	. /tmp/dietpi-globals
 	G_EXEC_NOHALT=1 G_EXEC rm /tmp/dietpi-globals
 fi
@@ -41,22 +41,22 @@ fi
 # Main
 G_EXEC cd /tmp
 G_EXEC curl -sSfLO "https://github.com/MichaIng/DietPi-Website/archive/$BRANCH.tar.gz"
-G_EXEC tar xf $BRANCH.tar.gz
-G_EXEC_NOHALT=1 G_EXEC rm $BRANCH.tar.gz
-G_EXEC cd DietPi-Website-$BRANCH
+G_EXEC tar xf "$BRANCH.tar.gz"
+G_EXEC_NOHALT=1 G_EXEC rm "$BRANCH.tar.gz"
+G_EXEC cd "DietPi-Website-$BRANCH"
 # Cleanup
 [[ $GITHUB_ACTIONS ]] || G_EXEC_NOHALT=1 G_EXEC rm -R README.md LICENSE deploy.bash .??*
 # Update sitemap timestamps
 G_EXEC sed -i "s|<lastmod>.*</lastmod>|<lastmod>$(date '+%Y-%m-%dT%T%:z')</lastmod>|" sitemap.xml
 
 # 3rd party
-G_EXEC curl -sSfL 'https://raw.githubusercontent.com/jquery/codeorigin.jquery.com/main/cdn/jquery-3.6.0.min.js' -o js/jquery.js
-G_EXEC curl -sSfL 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' -o css/bootstrap.css
-G_EXEC curl -sSfL 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' -o js/bootstrap.js
+G_EXEC curl -sSf 'https://raw.githubusercontent.com/jquery/codeorigin.jquery.com/main/cdn/jquery-3.6.0.min.js' -o js/jquery.js
+G_EXEC curl -sSf 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' -o css/bootstrap.css
+G_EXEC curl -sSf 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' -o js/bootstrap.js
 
 # Minify
 # - Download
-G_EXEC curl -sSfL "$(curl -sSfL 'https://api.github.com/repos/tdewolff/minify/releases/latest' | mawk -F\" '/\"browser_download_url\".*linux_amd64\.tar\.gz\"$/{print $4}')" -o minify.tar.gz
+G_EXEC curl -sSfL "$(curl -sSf 'https://api.github.com/repos/tdewolff/minify/releases/latest' | mawk -F\" '/\"browser_download_url\".*linux_amd64\.tar\.gz\"$/{print $4}')" -o minify.tar.gz
 G_EXEC tar xf minify.tar.gz minify
 G_EXEC_NOHALT=1 G_EXEC rm minify.tar.gz
 # - Minify JavaScript
@@ -86,10 +86,10 @@ then
 	then
 		G_EXEC cp -R . "$TARGET_DIR/"
 		G_EXEC cd /tmp
-		G_EXEC rm -R DietPi-Website-$BRANCH
+		G_EXEC rm -R "DietPi-Website-$BRANCH"
 	else
 		G_EXEC cd /tmp
-		G_EXEC mv DietPi-Website-$BRANCH "$TARGET_DIR"
+		G_EXEC mv "DietPi-Website-$BRANCH" "$TARGET_DIR"
 	fi
 fi
 }
