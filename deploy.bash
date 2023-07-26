60a6745b873f7d758bef62e6dda6138d7ef63a3e
MichaIng <micha@dietpi.com>
2023-07-26 16:32:06 +0200

    Update Bootstrap to v5.3.1

--- deploy.bash
+++ deploy.bash
@@ -69,8 +69,8 @@ G_EXEC cd "DietPi-Website-$BRANCH"
 G_EXEC sed -i "s|<lastmod>.*</lastmod>|<lastmod>$(date '+%Y-%m-%dT%T%:z')</lastmod>|" sitemap.xml
 
 # Bootstrap
-G_EXEC curl -sSf 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' -o css/bootstrap.css
-G_EXEC curl -sSf 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js' -o js/bootstrap.js
+G_EXEC curl -sSf 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' -o css/bootstrap.css
+G_EXEC curl -sSf 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js' -o js/bootstrap.js
 
 # Minify
 # - Download
